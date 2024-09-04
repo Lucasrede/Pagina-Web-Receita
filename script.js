3b7a8c32b8b6e83458a7d7987e76c6dfe24b2098
Lucas Lourenço Santos <115506713+Lucasrede@users.noreply.github.com>
2024-09-04 20:19:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+//Aqui vem o código JS(abreviação de Javascript).
+
+  function alterarImagem(img, 'avocado+toast+1-removebg-preview.png'){
+    document.getElementById(img).src = 'avocado.png'
+
+  }
+</script>
